1e0ff30c2b39069b311772d726b94fcbf8db50ee
lukskalashs <kalashslukhanyo@gmail.com>
2024-07-08 15:05:40 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Tempreture_Conversion_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBoxType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(113, 28);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(313, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "TEMPRETURE CONVERSION APP";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(29, 72);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 1;
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.AutoSize = true;
+            this.lblLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLabel.Location = new System.Drawing.Point(52, 90);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(68, 16);
+            this.lblLabel.TabIndex = 2;
+            this.lblLabel.Text = "Degrees";
+            this.lblLabel.Click += new System.EventHandler(this.lblLabel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type";
+            // 
+            // cmbBoxType
+            // 
+            this.cmbBoxType.DropDownHeight = 102;
+            this.cmbBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBoxType.FormattingEnabled = true;
+            this.cmbBoxType.IntegralHeight = false;
+            this.cmbBoxType.Items.AddRange(new object[] {
+            "Celsius - Fahrenheit",
+            "Fahrenheit - Celsius",
+            "Kelvin - Celsius",
+            "Celsius - Kelvin",
+            "Fahrenheit - Kelvin",
+            "Kelvin - Fahrenheit"});
+            this.cmbBoxType.Location = new System.Drawing.Point(393, 109);
+            this.cmbBoxType.Name = "cmbBoxType";
+            this.cmbBoxType.Size = new System.Drawing.Size(150, 21);
+            this.cmbBoxType.TabIndex = 5;
+            this.cmbBoxType.SelectedIndexChanged += new System.EventHandler(this.cmbBoxType_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(237, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Results";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(183, 230);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(191, 41);
+            this.lblResults.TabIndex = 7;
+            this.lblResults.Click += new System.EventHandler(this.lblResults_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(220, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(598, 301);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBoxType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblLabel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBoxType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tempreture_Conversion_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Methods for Conversion
+        public double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 9 / 5) + 32;
+        }
+        public double FahrenheitToCelsius(double Fahrenheit)
+        {
+            return (Fahrenheit - 32) * 5 / 9;
+        }
+        public double CelsiusToKelvin(double Celsius)
+        {
+            return Celsius + 273.15;
+        }
+        public double KelvinToCelsius(double Kelvin)
+        {
+            return Kelvin - 273.15;
+        }
+        public double FahrenheitToKelvin(double Fahrenheit)
+        {
+            return (Fahrenheit - 32) * 5 / 9 + 273.15;
+        }
+        public double KelvinToFahrenheit(double Kelvin)
+        {
+            return (Kelvin - 273.15) * 9 / 5 + 32;
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResults_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblLabel_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void cmbBoxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = cmbBoxType.SelectedItem.ToString();
+            if (selectedItem == "Celsius - Fahrenheit" || selectedItem == "Fahrenheit - Celsius" || selectedItem == "Celsius - Kelvin" || selectedItem == "Fahrenheit - Kelvin")
+            {
+                lblLabel.Text = "Degrees";
+            }
+            else if (selectedItem == "Kelvin - Celsius" || selectedItem == "Kelvin - Fahrenheit")
+            {
+                lblLabel.Text = "Kelvin";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cmbBoxType.SelectedItem != null)
+            {
+                try
+                {
+                    string SelectedItem = cmbBoxType.SelectedItem.ToString();
+                    double TempToChange = double.Parse(textBox1.Text.ToString());
+                    if (SelectedItem == "Celsius - Fahrenheit")
+                    {
+                        double Fahrenheit = CelsiusToFahrenheit(TempToChange);
+
+                        lblResults.Text = Fahrenheit.ToString().PadLeft(18);
+                    }
+                    if (SelectedItem == "Fahrenheit - Celsius")
+                    {
+                        double Celsius = FahrenheitToCelsius(TempToChange);
+                        lblResults.Text = Celsius.ToString().PadLeft(18);
+                    }
+                    if (SelectedItem == "Kelvin - Celsius")
+                    {
+                        double Kelvin = KelvinToCelsius(TempToChange);
+                        lblResults.Text = Kelvin.ToString().PadLeft(18);
+                    }
+                    if (SelectedItem == "Celsius - Kelvin")
+                    {
+                        double Celsius = CelsiusToKelvin(TempToChange);
+                        lblResults.Text = Celsius.ToString().PadLeft(18);
+                    }
+                    if (SelectedItem == "Fahrenheit- Kelvin")
+                    {
+                        double Fahrenheit = FahrenheitToKelvin(TempToChange);
+                        lblResults.Text = Fahrenheit.ToString().PadLeft(18);
+                    }
+                    else if (SelectedItem == "Kelvin - Fahrenheit")
+                    {
+                        double Kelvin = KelvinToFahrenheit(TempToChange);
+                        lblResults.Text = Kelvin.ToString().PadLeft(18);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Select a Conversion from the drop down");
+            }
+            
+        }
+    }
+}
+
+
+//Celsius - Fahrenheit
+//Fahrenheit - Celsius
+//Kelvin - Celsius
+//Celsius - Kelvin
+//Fahrenheit - Kelvin
+//Kelvin - Fahrenheit 
